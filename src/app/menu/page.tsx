755811d122aa7d293efc4fe096a8b7a07fc721fa
otodavid <ojooloruntomi@gmail.com>
2024-05-28 23:58:11 -0500

    added skeletal info for menu page

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,7 +1,55 @@
+import Image from 'next/image';
+
 export default function Menu() {
-    return (
+  return (
+    <div>
+      <h1>Menu</h1>
+
+      {/* Drinks section */}
+      <section>
+        <h2>Drinks</h2>
+
         <div>
-            <h1>Menu</h1>
+          <div>
+            <div>
+              <Image src='' alt='' />
+            </div>
+            <p>Hot coffee</p>
+          </div>
+          <div>
+            <div>
+              <Image src='' alt='' />
+            </div>
+            <p>Cold coffee</p>
+          </div>
+          <div>
+            <div>
+              <Image src='' alt='' />
+            </div>
+            <p>Bottle Beverages</p>
+          </div>
         </div>
-    )
+      </section>
+
+      {/* Drinks section */}
+      <section>
+        <h2>Food</h2>
+
+        <div>
+          <div>
+            <div>
+              <Image src='' alt='' />
+            </div>
+            <p>Bakery</p>
+          </div>
+          <div>
+            <div>
+              <Image src='' alt='' />
+            </div>
+            <p>Snacks and sweets</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
