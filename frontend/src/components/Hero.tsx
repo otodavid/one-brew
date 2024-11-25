28acee0c1488da835e85dc5d858c56d8fb78d033
David Ojo <ojooloruntomi@gmail.com>
2024-11-25 10:35:00 -0600

    optimized hero image

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,17 +1,21 @@
 import Image from 'next/image';
-import heroPic from '/public/img/hero-coffee.png';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+import heroPic from '/public/img/test.jpg';
 
 export default function Hero() {
+
   return (
     <section className='relative isolate'>
-      <div className='min-h-[30rem] grid items-center sm:grid sm:grid-cols-[1fr_.5fr] bg-gradient-to-tr sm:from-black/85 sm:from-40% sm:to-transparent sm:to-80% xl:py-24 '>
+      <div className='min-h-[30rem] grid items-center bg-gradient-to-tr sm:from-black/85 sm:from-40% sm:to-transparent sm:to-80% sm:grid sm:grid-cols-[1fr_.5fr] xl:py-24 '>
         <Image
-          src={'/img/test2.jpg'}
+          src={heroPic}
           alt='brown hero background'
           fill={true}
+          sizes="(max-width: 767px) 40vw, (max-width: 1200px) 50vw, 100vw"
+          priority={true}
           className='-z-10 object-cover brightness-[.2] sm:brightness-100'
+          placeholder='blur'
         />
         <div className='text-center text-background mx-auto py-20 px-4 xs:px-8 sm:px-6 sm:text-left md:px-12 lg:max-w-2xl'>
           <h1 className='font-bold capitalize'>
